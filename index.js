7109e3ca58462881192b33368c1b9597290a1e4b
1804011 <sharifsharifulislam061@gmail.com>
2022-04-25 22:40:58 +0600

    env var added

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-	"mongodb+srv://sharif:121418116@cluster0.azxym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.azxym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
